60b3515b25cb0b79f1ec9c12a52e20c53abd0ec7
John Moir <jmmoir@outlook.com>
2024-05-25 20:19:14 +0100

    add hamburger menu

--- src/app/app.component.header.ts
+++ src/app/app.component.header.ts
@@ -1,42 +1,11 @@
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger
-} from "@angular/animations";
 import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-header',
     templateUrl: 'app.component.header.html',
-    animations: [
-      trigger("openClose", [
-        // ...
-        state(
-          "open",
-          style({
-            opacity: 1,
-            transform: "scale(1, 1)"
-          })
-        ),
-        state(
-          "closed",
-          style({
-            opacity: 0,
-            transform: "scale(0.95, 0.95)"
-          })
-        ),
-        transition("open => closed", [animate("100ms ease-in")]),
-        transition("closed => open", [animate("200ms ease-out")])
-      ])
-    ]
 })
 export class HeaderComponent {
-  showMobileMenu = false;
-  get openCloseTrigger() {
-    return this.showMobileMenu ? "open" : "closed";
-  }
+  showMobileMenu = true;
   toggleMobileMenu(){
     this.showMobileMenu = !this.showMobileMenu;
   }
