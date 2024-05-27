55fd6542541024ca247a6801ee4dc22c94140d76
John Moir <jmmoir@outlook.com>
2024-05-27 14:00:32 +0100

    base file setup for hero banner

---
+++ src/app/app.component.heroBanner.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-heroBanner',
+    templateUrl: 'app.component.heroBanner.html',
+})
+export class HeroBannerComponent {
+}
