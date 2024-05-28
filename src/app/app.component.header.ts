784430d6c94c3dcf579c0e3b4474246451407528
John Moir <jmmoir@outlook.com>
2024-05-28 14:17:30 +0100

    routing resolved

--- src/app/app.component.header.ts
+++ src/app/app.component.header.ts
@@ -1,12 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-header',
     templateUrl: 'app.component.header.html',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(private router: Router) { }
+  ngOnInit(): void {
+  }
   showMobileMenu = true;
   toggleMobileMenu(){
     this.showMobileMenu = !this.showMobileMenu;
+  }
+
+  navigateToAboutMe(){
+    this.router.navigateByUrl('/aboutMe');
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./app.component.header";
 import { HeroBannerComponent } from "./app.component.heroBanner";
 import { FooterComponent } from "./app.component.footer";
+import { AboutMeComponent } from "./app.component.aboutMe";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HeroBannerComponent, FooterComponent],
-  imports: [BrowserModule],
-  bootstrap: [AppComponent, HeaderComponent, HeroBannerComponent, FooterComponent]
+  declarations: [AppComponent, HeaderComponent, HeroBannerComponent, FooterComponent, AboutMeComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  bootstrap: [AppComponent, HeaderComponent, HeroBannerComponent, FooterComponent, AboutMeComponent]
 })
 export class AppModule {}
