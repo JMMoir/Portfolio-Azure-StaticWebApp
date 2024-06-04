451a9d3e51f3552f7adcc11894c206fd2f7d1d43
John Moir <jmmoir@outlook.com>
2024-06-04 22:07:34 +0100

    projects landing page

--- src/app/app.component.projects.ts
+++ src/app/app.component.projects.ts
@@ -5,4 +5,7 @@ import { Component } from "@angular/core";
     templateUrl: 'app.component.projects.html'
 })
 export class ProjectsComponent{
+    toggleSlideOver() {
+        console.log('mouse over working')
+    }
 }
